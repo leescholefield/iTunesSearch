4e7555fdaf267be3a2ad99a331d2d6145c3b1f5c
Lee Scholefield <leescholefield@gmail.com>
2017-04-23 23:56:26 +0100

    Added documentation

--- src/main/Request.java
+++ src/main/Request.java
@@ -4,10 +4,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Represents a main.Request to the iTunes Search Api.
+ * Represents an http Request to the iTunes Search Api.
  */
 public class Request {
 
+    /* Valid keys for the request url. */
     public enum Keys {
         TERM,
         COUNTRY,
@@ -35,6 +36,9 @@ public class Request {
         paramMap = requestParams;
     }
 
+    /**
+     * Creates a String url from the values in the paramMap.
+     */
     public String createUrl(){
         final String urlBase = "https://itunes.apple.com/search?";
 
@@ -49,17 +53,26 @@ public class Request {
     }
 
 
+    /**
+     * Factory class for Request.
+     */
     public static class RequestFactory {
 
         private Map<Request.Keys, String> paramMap = new HashMap<>();
 
+        /* Creates a {@link Request} */
         public Request newRequest(){
             return new Request(paramMap);
         }
 
+        /* term is the only required value for a Request */
         public RequestFactory(String term){
             this.paramMap.put(Request.Keys.TERM, term);
         }
+
+        /**
+         * Constructor methods.
+         */
 
         public RequestFactory country(String country){
             this.paramMap.put(Request.Keys.COUNTRY, country);
@@ -81,6 +94,10 @@ public class Request {
             return this;
         }
 
+        /**
+         * Number of results for the itunes request to return.
+         * @param limit must be between 0 and 200.
+         */
         public RequestFactory limit(int limit){
             if(limit >= 0 && limit <= 200){
                 this.paramMap.put(Request.Keys.LIMIT, String.valueOf(limit));
@@ -94,6 +111,10 @@ public class Request {
             return this;
         }
 
+        /**
+         * The results version to be returned by the itunes request.
+         * @param version either 1 or 2
+         */
         public RequestFactory version(int version){
             if(version == 1 || version == 2){
                 this.paramMap.put(Request.Keys.VERSION, String.valueOf(version));
@@ -102,6 +123,9 @@ public class Request {
             throw new IllegalArgumentException("version must either be 1 or 2");
         }
 
+        /**
+         * Whether explicit content should be included in the returned results.
+         */
         public RequestFactory explicit(boolean explicit){
             String val = "Yes";
             if(!explicit){
