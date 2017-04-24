5bc3c2a8f6d016f883cacf8138b8c5ec25bcf5f9
Lee Scholefield <leescholefield@gmail.com>
2017-04-24 22:36:57 +0100

    Initial commit.

--- src/main/SimpleHttpRequest.java
+++ src/main/SimpleHttpRequest.java
@@ -1,7 +1,29 @@
 package main;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 /**
- * Created by lee on 24/04/17.
+ *  Used by the Request class as the default way to send a request to the iTunes Search Api.
  */
-public class SimpleHttpRequest {
+class SimpleHttpRequest {
+
+    public Response sendRequest(Request request) throws MalformedURLException, IOException{
+        URL url = new URL(request.createUrl());
+        InputStream is = url.openStream();
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder result = new StringBuilder();
+        String line;
+
+        while((line = reader.readLine()) != null){
+            result.append(line);
+        }
+
+        return new Response(request.getParamMap(), result.toString());
+    }
 }
