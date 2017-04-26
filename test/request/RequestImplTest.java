8172620133a1a14c36990090465d68a94aa519f3
Lee Scholefield <leescholefield@gmail.com>
2017-04-26 15:01:25 +0100

    Added documentation explaining use of static protected createUrl() rather than public createUrl()

--- test/request/RequestImplTest.java
+++ test/request/RequestImplTest.java
@@ -22,6 +22,11 @@ public class RequestImplTest {
     }
 
 
+    /**
+     * Tests the private implementation of createUrl(), rather than the public parameter-less version, because RequestImpl
+     * uses a HashMap to store the Keys and their values which does not preserve insertion order. This makes it difficult
+     * to test String equality.
+     */
     @Test
     public void createUrl() throws Exception {
         String expected = "https://itunes.apple.com/search?&term=history&media=podcast&limit=150&explicit=No&offset=0";
