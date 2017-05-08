b8d771a594f004be38b00afd62411ffec46ba2bc
Lee Scholefield <leescholefield@gmail.com>
2017-05-08 13:53:21 +0100

    Initial Artist model commit

---
+++ src/model/artist/Artist.java
@@ -0,0 +1,48 @@
+package model.artist;
+
+import model.Item;
+
+/**
+ * Represents an Artist.
+ */
+public class Artist extends Item {
+
+    private String artistLinkUrl;
+    private String artistName;
+    private String artistType;
+
+    private long artistId;
+    private long amgArtistId;
+
+    private String primaryGenreName;
+    private long primaryGenreId;
+
+
+    public String getArtistLinkUrl() {
+        return artistLinkUrl;
+    }
+
+    public String getArtistName() {
+        return artistName;
+    }
+
+    public String getArtistType() {
+        return artistType;
+    }
+
+    public long getArtistId() {
+        return artistId;
+    }
+
+    public String getPrimaryGenreName() {
+        return primaryGenreName;
+    }
+
+    public long getPrimaryGenreId() {
+        return primaryGenreId;
+    }
+
+    public long getAmgArtistId() {
+        return amgArtistId;
+    }
+}
