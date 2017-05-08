cf419a529d24cebace61ecbf4315054e1f26957d
Lee Scholefield <leescholefield@gmail.com>
2017-05-08 14:44:55 +0100

    Added switch statement to deal with different model types.

--- src/parse/ItemDeserializer.java
+++ src/parse/ItemDeserializer.java
@@ -1,6 +1,13 @@
 package parse;
 
 import com.google.gson.*;
+import model.Item;
+import model.artist.Artist;
+import model.audiobook.Audiobook;
+import model.track.FeatureMovie;
+import model.track.MusicVideo;
+import model.track.Podcast;
+import model.track.Song;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
@@ -20,10 +27,40 @@ public class ItemDeserializer implements JsonDeserializer<List<Item>> {
 
         for (JsonElement element : resultsArray){
             String wrapperType = element.getAsJsonObject().get("wrapperType").getAsString();
+            Class<? extends Item> c = null;
 
-            if ("track".equals(wrapperType)){
-                Item track = gson.fromJson(element, Item.class);
-                itemList.add(track);
+            switch(wrapperType){
+
+                case "track":
+                    String kind = element.getAsJsonObject().get("kind").getAsString();
+                    switch(kind){
+                        case "song":
+                            c = Song.class;
+                            break;
+                        case "music-video":
+                            c = MusicVideo.class;
+                            break;
+                        case "feature-movie":
+                            c = FeatureMovie.class;
+                            break;
+                        case "podcast":
+                            c = Podcast.class;
+                    }
+                    break;
+                case "artist":
+                    c = Artist.class;
+                    break;
+                case "audiobook":
+                    c = Audiobook.class;
+                    break;
+            } // end of switch
+
+
+            if (c != null){
+                Item item = gson.fromJson(element, c);
+                itemList.add(item);
+            } else{
+                throw new IllegalArgumentException("Not a recognized wrapperType");
             }
         }
 
