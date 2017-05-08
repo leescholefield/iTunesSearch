b15a9a4b6f1e6db5c3f3b75d8a139bd30f9c4124
Lee Scholefield <leescholefield@gmail.com>
2017-05-08 22:06:30 +0100

    Added ItemType enum and getType abstract method.

--- src/model/Item.java
+++ src/model/Item.java
@@ -4,7 +4,18 @@ package model;
 /**
  * Base class for a deserialized result.
  */
-public class Item {
+public abstract class Item {
+
+    protected enum ItemType{
+        SONG,
+        MUSIC_VIDEO,
+        PODCAST,
+        FEATURE_MOVIE,
+        ARTIST,
+        AUDIOBOOK
+    }
+
+    protected abstract ItemType getType();
 
 
 }
