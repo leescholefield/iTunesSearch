1b7287fcf3230ac8f95f81ee74172ed8da715483
Lee Scholefield <leescholefield@gmail.com>
2017-05-14 13:17:38 +0100

    Added parseToModel() method, added documentation.

--- src/parse/Parser.java
+++ src/parse/Parser.java
@@ -4,6 +4,7 @@ package parse;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
+import model.Item;
 
 import java.lang.reflect.Type;
 import java.util.List;
@@ -14,12 +15,32 @@ import java.util.List;
  */
 public class Parser {
 
-    public List<Item> parse(String jsonString){
+    /**
+     * Parses the given JSON string into a ResultList of Item instances.
+     *
+     * @param jsonString json string to parse.
+     * @return ResultList of type Item instance.
+     */
+    public ResultList<Item> parse(String jsonString){
         Type type = new TypeToken<List<Item>>(){}.getType();
         Gson gson = new GsonBuilder().registerTypeAdapter(type, new ItemDeserializer())
                 .create();
 
-        return gson.fromJson(jsonString, type);
+        return new ResultList<>(gson.fromJson(jsonString, type));
+    }
+
+    /**
+     * Utility method for automatically parsing a JSON string into Item subclasses.
+     *
+     * @param jsonString json string to parse.
+     * @param type ItemType to search for.
+     * @param model Item subclasses to cast to.
+     * @param <T> model to cast Item instances to.
+     * @return a new ResultList of type T.
+     */
+    public <T extends Item> ResultList<T> parseToModel(String jsonString, Item.ItemType type, Class<T> model){
+        ResultList<Item> itemResults = parse(jsonString);
+        return itemResults.getResultsOfType(type, model);
     }
 
 
