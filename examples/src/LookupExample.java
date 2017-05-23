b4be61858a54d1b45d3750f05018070bfabd3683
Lee Scholefield <leescholefield@gmail.com>
2017-05-23 13:46:16 +0100

    Added multiple artists example.

--- examples/src/LookupExample.java
+++ examples/src/LookupExample.java
@@ -3,7 +3,7 @@ import model.artist.Artist;
 import model.track.Song;
 import parse.Parser;
 import parse.ResultList;
-import request.KeyVals;
+import request.RequestKeys;
 import request.LookupBuilder;
 import request.Request;
 import request.Response;
@@ -11,7 +11,7 @@ import request.Response;
 import java.io.IOException;
 
 /**
- * A simple Lookup example
+ * Contains different lookup examples.
  */
 public class LookupExample {
 
@@ -40,7 +40,7 @@ public class LookupExample {
      */
     private static ResultList<Song> topSongs() {
         long id = 178834;
-        Request request = new LookupBuilder(LookupBuilder.IdTypes.ITUNES, id).entity(KeyVals.Entity.Music.SONG)
+        Request request = new LookupBuilder(LookupBuilder.IdTypes.ITUNES, id).entity(RequestKeys.Entity.Music.SONG)
                 .limit(10).newRequest();
 
         try {
@@ -54,9 +54,40 @@ public class LookupExample {
 
     }
 
+    /**
+     * Lookups multiple artist ids and returns their 10 most recent songs(including songs they are featured on).
+     * <p>
+     * The actual result array will contain 33 items. 3 artist items for each id, and then 10 song items for each artist.
+     */
+    private static ResultList<Item> multipleArtistsSongs() {
+        Request request = new LookupBuilder(LookupBuilder.IdTypes.ITUNES, 178834, 909253, 289550)
+                .entity(RequestKeys.Entity.Music.SONG)
+                .limit(10)
+                .sort(RequestKeys.Sort.RECENT)
+                .newRequest();
+
+        try {
+            Response response = request.sendRequest();
+            return new Parser().parse(response);
+        } catch (IOException e){
+            System.exit(1);
+            // unreachable
+            return null;
+        }
+    }
+
     public static void main (String[] args){
-        ResultList<Song> results = topSongs();
-        for (Song song : results.getResultList()){
+        ResultList<Item> results = multipleArtistsSongs();
+        ResultList<Artist> artists = results.getResultsOfType(Item.ItemType.ARTIST, Artist.class);
+        ResultList<Song> songs = results.getResultsOfType(Item.ItemType.SONG, Song.class);
+
+        System.out.println("Artists:");
+        for (Artist artist : artists.getResultList()){
+            System.out.println(artist.toString());
+        }
+
+        System.out.println("Songs");
+        for (Song song : songs.getResultList()) {
             System.out.println(song.toString());
         }
     }
